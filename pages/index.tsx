a211e9df458fb7f3b312d941112fa9a87aec13bf
tzztson <zhenghu61919@gmail.com>
2022-12-16 14:08:32 +0800

    feat:updated the paypal-checkout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,24 @@
-export default function Home() {
-  return <h1 className="">Hello world!</h1>;
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+
+export default function App() {
+  return (
+    <div className="flex justify-center items-center h-[100vh]">
+      <PayPalScriptProvider
+        options={{
+          "client-id":
+            "AcwCr08xgyuunHb6wDbf06DZ16rdD-TLTW8YNEwo24kJj7Exfy_gohM9AWgUdbFw7InLaEjHtMsN7AYh",
+        }}
+      >
+        <PayPalButtons
+          style={{
+            layout: "horizontal",
+            color: "gold",
+            shape: "pill",
+            height: 55,
+            label: "paypal",
+          }}
+        />
+      </PayPalScriptProvider>
+    </div>
+  );
 }
