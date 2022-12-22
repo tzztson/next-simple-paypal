41d6aa1d3ba4e2e43d61f8cb218e8654c26dd30f
tzztson <zhenghu61919@gmail.com>
2022-12-23 05:16:21 +0800

    feat: complete the simple-paypal-checkout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,55 @@
+import { useState } from "react";
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
 export default function App() {
+  const [total, setTotal] = useState<number>(0);
   return (
-    <div className="flex justify-center items-center h-[100vh]">
-      <PayPalScriptProvider
-        options={{
-          "client-id":
-            "AcwCr08xgyuunHb6wDbf06DZ16rdD-TLTW8YNEwo24kJj7Exfy_gohM9AWgUdbFw7InLaEjHtMsN7AYh",
-        }}
-      >
-        <PayPalButtons
-          style={{
-            layout: "horizontal",
-            color: "gold",
-            shape: "pill",
-            height: 55,
-            label: "paypal",
-          }}
+    <div className="flex justify-center">
+      <div className="flex flex-col justify-center items-center gap-8 h-[100vh] px-24 ">
+        <input
+          type="text"
+          className="w-full pl-4 py-2 outline-none border-2 border-blue-400 focus:border-blue-600 rounded-lg font-bold text-2xl"
+          value={total}
+          onChange={(e) => setTotal(e.target.value)}
         />
-      </PayPalScriptProvider>
+        <PayPalScriptProvider
+          options={{
+            "client-id":
+              "AeeXSsrMzJT4wIV5uWdZELlqce9ixxNtdjCbGKjtSJ8yrqz1UVtvQ0jFRj-oZNs8f-G8PojBzQ63MOE6",
+          }}
+          className="w-full"
+        >
+          <PayPalButtons
+            forceReRender={[total]}
+            style={{
+              layout: "horizontal",
+              color: "gold",
+              shape: "pill",
+              label: "paypal",
+              height: 55,
+            }}
+            createOrder={(data, actions) => {
+              let price = total * 1.5;
+              return actions.order.create({
+                purchase_units: [
+                  {
+                    amount: {
+                      value: price.toString(),
+                      currency: "usd",
+                    },
+                  },
+                ],
+              });
+            }}
+            onApprove={(data, actions) => {
+              return actions.order.capture().then((details) => {
+                const name = details.payer.name.given_name;
+                alert(`Transaction completed by ${name}`);
+              });
+            }}
+          />
+        </PayPalScriptProvider>
+      </div>
     </div>
   );
 }
